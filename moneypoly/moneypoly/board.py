8ce02918ccf53fd73c50514b5c19616f99f9f802
Nandini <nandinichakaravarthy@gmail.com>
2026-03-22 17:27:32 +0530

    Iteration 2: added module docstring and fixed singleton comparison warning in board.py

--- moneypoly/moneypoly/board.py
+++ moneypoly/moneypoly/board.py
@@ -1,3 +1,5 @@
+"""Board module defining tiles, properties, and board-level queries."""
+
 from moneypoly.property import Property, PropertyGroup
 from moneypoly.config import (
     JAIL_POSITION,
@@ -105,7 +107,7 @@ class Board:
         prop = self.get_property_at(position)
         if prop is None:
             return False
-        if prop.is_mortgaged == True:
+        if prop.is_mortgaged:
             return False
         return prop.owner is None
 
